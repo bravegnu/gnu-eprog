17a62595525a44b8e2235dfbc255256cb4db3270
Vijay Kumar <vijaykumar@bravegnu.org>
2009-05-24 17:38:08 +0530

    Fix startup code to handle special case in which data section size is zero and/or bss section size is zero.

--- code/startup.s
+++ code/startup.s
@@ -14,22 +14,33 @@ start:
 	ldr   r0, =flash_sdata
 	ldr   r1, =ram_sdata
 	ldr   r2, =data_size
+
+	@@ Handle data_size == 0
+	cmp   r2, #0
+	beq   init_bss
 copy:
-	ldrb  r4, [r0], #1
-	strb  r4, [r1], #1
-	subs  r2, r2, #1
-	bne   copy
-	
+	ldrb   r4, [r0], #1
+	strb   r4, [r1], #1
+	subs   r2, r2, #1
+	bne    copy
+
+init_bss:
 	@@ Initialize .bss
 	ldr   r0, =sbss
 	ldr   r1, =ebss
 	ldr   r2, =bss_size
+
+	@@ Handle bss_size == 0
+	cmp   r2, #0
+	beq   init_stack
+	
 	mov   r4, #0
 zero:	
 	strb  r4, [r0], #1
 	subs  r2, r2, #1
 	bne   zero
-	
+
+init_stack:
 	@@ Initialize the stack pointer
 	ldr   sp, =0xA4000000
 
