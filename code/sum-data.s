17331359893c31639628ac5124a76d4e279a269f
Vinoth Kumar <vinothg.cdm@gmail.com>
2017-02-27 12:17:20 +0530

    Update sum-data.s
    Add parentheses over pointer expression

--- code/sum-data.s
+++ code/sum-data.s
@@ -7,7 +7,7 @@ start:
 	ldr   r0, =eoa	 	@ r0 = &eoa
 	ldr   r1, =arr		@ r1 = &arr
 	mov   r3, #0		@ r3 = 0
-loop:	ldrb  r2, [r1], #1	@ r2 = *r1++
+loop:	ldrb  r2, [r1], #1	@ r2 = *(r1++)
 	add   r3, r2, r3	@ r3 += r2
 	cmp   r1, r0		@ if (r1 != r2)
 	bne   loop		@    goto loop
